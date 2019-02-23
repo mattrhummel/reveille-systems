5498a986dbcdf4b1cdae367d7e323298656c61e9
Matt Hummel <matt@matthummel.co>
2019-02-23 11:21:45 -0500

    Update content-case-studies.php
    restore from wrong section copy error

---
+++ template-parts/home/content-case-studies.php
@@ -0,0 +1,83 @@
+						<!--case studies-->
+						<div class="case-studies">
+							<div class="container pb-md-3">
+								<div class="row">
+									<div class="col-sm-12 col-md-10">
+										<span class="h2 text-primary subheader">Proven Results</span>
+										<h2 class="h1 font-weight-bold">Case Studies</h2>
+										<p class="font-weight-normal pr-md-5 w-75">Reveille Systems takes a proactive approach to monitoring and managing network security within your technology environment. This proactive approach includes IT security audits and assessments to identify potential vulnerabilities in your IT network and remediate those vulnerabilities before the compromise your network or data.</p>
+										<a href="#" type="button" class="btn h6 callto-button font-weight-bold">learn more<i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+									</div>
+								</div>
+							</div>
+							<div class="container-fluid">
+								<div class="row case-slider mb-md-5">
+									<div class="col-sm-12 col-md-11 offset-md-1 pr-md-0">
+										<div class="owl-2 owl-carousel owl-theme pt-md-4">
+											<a href="">
+												<div class="item">
+													<div class="card shadow">
+														<div class="gradient-overlay"></div>
+														<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/img/case-study-placeholder.jpg?>" alt="case study">
+														<div class="card-img-overlay px-3">
+															<div class="card-text">
+																<h4 class="h7 text-uppercase text-white">subheader here</h4>
+																<h6 class="h6 text-capitalize text-white">Goodall, Pelt & Carper</h6>
+															</div>
+															<div class="card-button">
+																<span type="button" class="btn h6 callto-button-light font-weight-bold text-capitalize p-0"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+															</div>
+														</div>
+													</div>
+												</div>
+											</a>
+											<a href="">
+												<div class="item">
+													<div class="card shadow">
+														<div class="gradient-overlay"></div>
+														<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/img/case-study-placeholder.jpg?>" alt="case study">								<div class="card-img-overlay px-3">
+															<div class="card-text">
+																<h4 class="h7 text-uppercase text-white">subheader here</h4>
+																<h6 class="h6 text-capitalize text-white">Goodall, Pelt & Carper</h6>
+															</div>
+															<div class="card-button">
+																<span type="button" class="btn h6 callto-button-light font-weight-bold text-capitalize p-0"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>										</div>
+															</div>
+														</div>
+													</div>
+												</a>
+												<a href="">
+													<div class="item">
+														<div class="card shadow">
+															<div class="gradient-overlay"></div>
+															<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/img/case-study-placeholder.jpg?>" alt="case study">									<div class="card-img-overlay px-3">
+																<div class="card-text">
+																	<h4 class="h7 text-uppercase text-white">subheader here</h4>
+																	<h6 class="h6 text-capitalize text-white">Goodall, Pelt & Carper</h6>
+																</div>
+																<div class="card-button">
+																	<span type="button" class="btn h6 callto-button-light font-weight-bold text-capitalize p-0"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>										</div>
+																</div>
+															</div>
+														</div>
+													</a>
+													<a href="">
+														<div class="item">
+															<div class="card shadow">
+																<div class="gradient-overlay"></div>
+																<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/img/case-study-placeholder.jpg?>" alt="case study">									<div class="card-img-overlay px-3">
+																	<div class="card-text">
+																		<h4 class="h7 text-uppercase text-white">subheader here</h4>
+																		<h6 class="h6 text-capitalize text-white">Goodall, Pelt & Carper</h6>
+																	</div>
+																	<div class="card-button">
+																		<span type="button" class="btn h6 callto-button-light font-weight-bold text-capitalize p-0"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>									</div>
+																	</div>
+																</div>
+															</div>
+														</a>
+													</div>
+												</div>
+											</div>
+										</div>
+										</div><!--.casestudies-->
